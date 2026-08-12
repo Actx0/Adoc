590d96c08ccf1f80de2ab4a717e09a0715587df1
v <hey@clivern.com>
2026-08-12 22:22:24 +0200

    docs: add crisp chat widget

---
+++ crisp.js
@@ -0,0 +1,9 @@
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "59723515-368c-44ea-8065-31a4c24a7273";
+
+(function () {
+  var script = document.createElement("script");
+  script.src = "https://client.crisp.chat/l.js";
+  script.async = true;
+  document.head.appendChild(script);
+})();
